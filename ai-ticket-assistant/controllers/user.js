c751e7ba424f941ce654a74b1fcd0e3438575752
Nabin <sharmanabin.0910@gmail.com>
2025-06-03 17:58:12 +0530

    Controller-LogOut

--- ai-ticket-assistant/controllers/user.js
+++ ai-ticket-assistant/controllers/user.js
@@ -41,3 +41,15 @@ export const login = async(req , res)=>{
         res.status(500).json({error : "Login Failed" , details : error.message});
     }
 } 
+export const logout = async(req , res)=>{
+    try{
+        const token = req.headers.authorization.split(" ")[1]
+        if(!token) return res.status(401).json({error : "Unauthorized"})
+        jwt.verify(token , process.env.JWT_SECRET , (err , decoded)=>{
+            if(err) return res.status(401).json({error : "Unauthorized"})
+        })
+    res.json({message : "Logout Successfully"})
+    }catch{ 
+        res.status(500).json({error : "Logout Failed" , details : error.message})
+    }
+}
