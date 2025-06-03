9bbcbf0844ab3463cc8e7c54d9a7c7686b9d0a59
Nabin <sharmanabin.0910@gmail.com>
2025-06-03 17:41:56 +0530

    SignUp Controller

--- ai-ticket-assistant/controllers/user.js
+++ ai-ticket-assistant/controllers/user.js
@@ -4,5 +4,25 @@ import User from "../models/user.js"
 import {inngest} from "../inngest/client.js"
 
 export const signup = async (req , res)=>{
+    const {email , password , skills = []} = req.body
+    try{
+        const hashed= bcrypt.hash(password, 10);
+        const user = await User.create({email , password:hashed , skills})
+        //Fire Inngest event
+        await inngest.send({
+            name : "user/signup",
+            data : {
+                email 
+            }
+        });
+        jwt.sign(
+            {_id : user.id , role : user.role},
+            process.env.JWT_SECRET);
+            res.json({user ,token})
+    }catch(error){
+        res.status(500).json({error : "Signup Failed" , detials : error.message})
+    }   
+}
+export const login = async(req , res)=>{
     
 }
