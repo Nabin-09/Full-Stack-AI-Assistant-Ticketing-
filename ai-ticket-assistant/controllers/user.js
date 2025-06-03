d44483f264448de589f88eec8ffb4c3690b0dcbd
Nabin <sharmanabin.0910@gmail.com>
2025-06-03 17:33:21 +0530

    Started to work on controllers

---
+++ ai-ticket-assistant/controllers/user.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt"
+import jwt from "jwt"
+import User from "../models/user.js"
+import {inngest} from "../inngest/client.js"
+
+export const signup = async (req , res)=>{
+    
+}
