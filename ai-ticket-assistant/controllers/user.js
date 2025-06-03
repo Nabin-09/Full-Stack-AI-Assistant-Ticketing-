b330b29e28d3944e30f44ed0d3c9ef325d0f3bd5
Nabin <sharmanabin.0910@gmail.com>
2025-06-03 17:49:41 +0530

    Login Controller is ready

--- ai-ticket-assistant/controllers/user.js
+++ ai-ticket-assistant/controllers/user.js
@@ -16,7 +16,7 @@ export const signup = async (req , res)=>{
             }
         });
         jwt.sign(
-            {_id : user.id , role : user.role},
+            {_id : user._id , role : user.role},
             process.env.JWT_SECRET);
             res.json({user ,token})
     }catch(error){
@@ -24,5 +24,20 @@ export const signup = async (req , res)=>{
     }   
 }
 export const login = async(req , res)=>{
-    
-}
+    const {email , password} = req.body
+    try{
+        const user = User.findOne({email})
+        if(!user) return res.status(401).json({error : "User not found"})
+        const isMatch = await bcrypt.compare(password , user.password);
+        if(!isMatch){
+            return res.status(401).json({error:"Invalid credentials"});
+        }
+        const token = jwt.sign(
+            {_id : user._id , role : user.role},
+            process.env.JWT_SECRET
+        ); 
+        return json({user , token});
+    }catch(error){
+        res.status(500).json({error : "Login Failed" , details : error.message});
+    }
+} 
